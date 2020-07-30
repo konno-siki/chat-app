7ff1a1f111d57d3d006a5b28ea2e2c816fe79660
konno-siki <sakurakanji@icloud.com>
2020-07-30 17:31:00 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
   resources :users, only: [:edit, :update]
-
-  get 'users/sign_up'
-
-  get 'users/sign_in'
+  resources :rooms, only: [:new, :create]
 end
