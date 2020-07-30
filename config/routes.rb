7b478686b396d320a92f37847fc59dd51341dd49
konno-siki <sakurakanji@icloud.com>
2020-07-30 14:07:24 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "messages#index"
+  resources :users, only: [:edit, :update]
 
   get 'users/sign_up'
 
